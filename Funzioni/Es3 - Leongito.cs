6121aef0ad85c521c51fff54fd3d2658e98d9dbd
Leongito-Nicolo <leongito.nicolo@gmail.com>
2024-12-21 18:04:40 +0100

    NL004 - Exercise 3 functions
    Third exercise of the argument functions done

---
+++ Funzioni/Es3 - Leongito.cs
@@ -0,0 +1,15 @@
+﻿public class Funzioni
+{
+    public static void Main(String[] args)
+    {
+        // 3. Scrivere una funzione che accetta un parametro string e restituisce la lunghezza della stringa.
+        Console.WriteLine("Insert the string to calculate length: ");
+        string a = Console.ReadLine();
+        Console.WriteLine("The length of the string is: " + StringLength(a));
+    }
+
+    public static int StringLength(string stringToCalculate)
+    {
+        return stringToCalculate.Length;
+    }
+}
