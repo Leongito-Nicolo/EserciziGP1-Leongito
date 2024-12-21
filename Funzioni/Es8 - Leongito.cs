7e092af022363cbf756a0eecce3799a2ca4bbf56
Leongito-Nicolo <leongito.nicolo@gmail.com>
2024-12-21 18:45:22 +0100

    NL009 - Exercise 8 functions
    Eight exercise of the argument functions done

---
+++ Funzioni/Es8 - Leongito.cs
@@ -0,0 +1,27 @@
+﻿public class Funzioni
+{
+    public static void Main(String[] args)
+    {
+        // 8. Creare una funzione che utilizza un'eccezione per gestire input non validi. (try - catch)
+        int n;
+        do
+        {
+            Console.WriteLine("Insert a number between 5 and 100: ");
+            n = Convert.ToInt32(Console.ReadLine());
+        } while (!isValid(n));
+    }
+
+    public static bool isValid(int num1)
+    {
+        try
+        {
+            if (num1 < 5 || num1 > 100)
+                throw new Exception();
+            return true;
+        }catch (Exception)
+        {
+            Console.WriteLine("The number is invalid!");
+            return false;
+        }
+    }
+}
