d3d9de2f19a44d5a8681d10c06a9a9c1d720c99f
Leongito-Nicolo <leongito.nicolo@gmail.com>
2024-12-21 18:21:14 +0100

    NL006 - Exercise 5 functions
    Fifth exercise of the argument functions done

---
+++ Funzioni/Es5 - Leongito.cs
@@ -0,0 +1,20 @@
+﻿public class Funzioni
+{
+    public static void Main(String[] args)
+    {
+        // 5. Scrivere una funzione che accetta un array come parametro.
+        int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Console.WriteLine("The sum of every number in the array is: " + SumOfElements(num));
+    }
+
+    public static int SumOfElements(int[] array)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+}
