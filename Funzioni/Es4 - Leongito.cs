3890f755e6d30df8d416fa2fce089253fe6a8733
Leongito-Nicolo <leongito.nicolo@gmail.com>
2024-12-21 18:16:48 +0100

    NL005 - Exercise 4 functions
    Fourth exercise of the argument functions done

---
+++ Funzioni/Es4 - Leongito.cs
@@ -0,0 +1,14 @@
+﻿public class Funzioni
+{
+    public static void Main(String[] args)
+    {
+        // 4. Dichiarare una funzione con un parametro opzionale.
+        Console.WriteLine("3 to the second power: " + Pow(3));
+        Console.WriteLine("4 to the third power: " + Pow(4, 3));
+    }
+
+    public static int Pow(int num, int pow = 2)
+    {
+        return (int)Math.Pow(num, pow);
+    }
+}
