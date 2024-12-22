6f2d8c3a5a060e828fe1017d4b7086d1704c7de4
Leongito-Nicolo <leongito.nicolo@gmail.com>
2024-12-22 15:21:36 +0100

    NL013 - Exercise 12 functions
    Twelfth exercise of the argument functions done

---
+++ Funzioni/Es12 - Leongito.cs
@@ -0,0 +1,22 @@
+﻿public class Funzioni
+{
+    public static void Main(String[] args)
+    {
+        // 12. Creare una funzione che utilizza un'espressione condizionale per determinare il valore restituito. (if else)
+        Console.WriteLine("Insert a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (IsDivisibleBySeven(num))
+            Console.WriteLine("The number " + num + " is divisible by 7");
+        else
+            Console.WriteLine("The number " + num + " is not divisible by 7");
+    }
+
+    public static bool IsDivisibleBySeven(int n)
+    {
+        if (n % 7 == 0)
+            return true;
+        else
+            return false;
+    }
+}
