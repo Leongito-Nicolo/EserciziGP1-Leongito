e1dc9376f7dc25c953ed8d672562501f4858c768
Leongito-Nicolo <leongito.nicolo@gmail.com>
2024-12-29 22:28:02 +0100

    NL022 - Exercises 4-5-6 loop
    Fourth, fifth and sixth exercises of the argument loop done

---
+++ Loop/Es04-05-06 - Leongito.cs
@@ -0,0 +1,30 @@
+﻿public class Loop
+{
+    public static void Main(String[] args)
+    {
+        //4.Scrivere un ciclo for che calcola la somma di un array.
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+        Console.WriteLine("Sum: " + sum);
+
+        //5.Utilizzare un ciclo foreach per iterare attraverso una lista di stringhe.
+        List<string> names = new List<string> { "Luca", "Sara", "Silvia" };
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+
+        //6.Creare un ciclo for che calcola il fattoriale di un numero.
+        int number = 5;
+        int factorial = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+        Console.WriteLine("Factorial: " + factorial);
+    }
+}
