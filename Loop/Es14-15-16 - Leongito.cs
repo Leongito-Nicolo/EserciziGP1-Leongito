fd4b3a91de1796d104c822575ab2f5e9c6bc8848
Leongito-Nicolo <leongito.nicolo@gmail.com>
2024-12-29 22:42:22 +0100

    NL025 - Exercises 14-15-16 loop
    Fourteenth, fifteenth and sixteenth exercises of the argument loop done

---
+++ Loop/Es14-15-16 - Leongito.cs
@@ -0,0 +1,32 @@
+﻿public class Loop
+{
+    public static void Main(String[] args)
+    {
+        //14.Utilizzare un ciclo while che termina con un'istruzione continue.
+        int number = 0;
+        while (number < 10)
+        {
+            number++;
+            if (number % 2 == 0) 
+                continue;
+            Console.WriteLine(number);
+        }
+
+        //15.Scrivere un ciclo for che utilizza una variabile dichiarata all'esterno del ciclo.
+        int sum = 0;
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i % 2 != 0) 
+                sum += i;
+        }
+        Console.WriteLine(sum);
+
+        //16.Creare un ciclo foreach che stampa gli indici e i valori di un array.
+        string[] items = { "apple", "banana", "cherry" };
+        foreach (string item in items)
+        {
+            Console.WriteLine(item.Length);
+        }
+
+    }
+}
