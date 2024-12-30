aa723d47b502eace076ff2c85d097a91d7de3074
Leongito-Nicolo <leongito.nicolo@gmail.com>
2024-12-30 15:44:23 +0100

    NL040 - Exercises 10-11-12 array and lists
    Tenth, eleventh and twelfth exercises of the argument array and lists done

---
+++ Array e liste/Es10-11-12 - Leongito.cs
@@ -0,0 +1,35 @@
+﻿public class ArrayListe
+{
+    public static void Main(String[] args)
+    {
+        //10.Dichiarare un array e inizializzarlo con valori predefiniti.
+        int[] numbers = new int[5] { 1, 2, 3, 4, 5 };
+        foreach (int number in numbers)
+        {
+            Console.WriteLine("Number: " + number);
+        }
+
+        //11.Creare una lista che accetta solo valori univoci.
+        HashSet<int> uniqueNumbers = new HashSet<int> { 1, 2, 3, 3, 4 };
+        foreach (int number in uniqueNumbers)
+        {
+            Console.WriteLine("Unique number: " + number);
+        }
+
+        //12.Utilizzare un array per rappresentare una matrice di 3x3.
+        int[,] matrix = new int[3, 3] {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        for (int rows = 0; rows < matrix.GetLength(0); rows++)
+        {
+            for (int cols = 0; cols < matrix.GetLength(1); cols++)
+            {
+                Console.Write(matrix[rows, cols] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
