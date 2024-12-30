5c3f378b5037c363cd232caf41d63479e319bb7f
Leongito-Nicolo <leongito.nicolo@gmail.com>
2024-12-30 13:05:36 +0100

    NL031 - Exercises 7-8-9 enum, struct, cast
    Seventh, eight and ninth exercises of the argument enum, struct, cast done

---
+++ Enum Struct Cast/Es07-08-09 - Leongito.cs
@@ -0,0 +1,48 @@
+﻿public class EnumsStructCast
+{
+    struct Circle
+    {
+        public double Radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Circumference()
+        {
+            return 2 * Math.PI * this.Radius;
+        }
+    }
+
+    enum Status
+    {
+        Pending = 1,
+        Approved = 2,
+        Rejected = 3
+    }
+
+    enum Colors 
+    { 
+        Red = 1, 
+        Yellow = 2, 
+        Blue = 3 
+    }
+
+    public static void Main(String[] args)
+    {
+        //7.Dichiarare una struct con un costruttore e un metodo.
+        Circle circle = new Circle(5);
+        Console.WriteLine("Circumference: " + circle.Circumference());
+
+        //8. Scrivere un programma che converte un valore int in un enum usando il casting.
+        int statusValue = 2;
+        Status status = (Status)statusValue;
+        Console.WriteLine("Status: " + status);
+
+        //9. Dichiarare un enum con valori personalizzati e verificarne l'uso. 
+        Colors code = Colors.Red;
+        Console.WriteLine("Color code: " + (int)code);
+
+    }
+}
